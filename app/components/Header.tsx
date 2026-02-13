1aa5ccc64d2555ac0ba246529cf0a6135be1709b
GreiceSchneider0103 <greicelessul@gmail.com>
2026-02-13 14:22:05 -0300

    fix: atualizacao header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -20,8 +20,7 @@ type Props = {
 
 export function Header({ isLoggedIn, userName }: Props) {
   return (
-    <header className="flex items-center justify-between px-6 py-4 mt-4">
-
+    <header className="flex items-center justify-between">
       {/* Logo */}
       <Link href="/" className="flex items-center gap-3">
         <div className="h-9 w-9 rounded-xl theme-logo grid place-items-center">
